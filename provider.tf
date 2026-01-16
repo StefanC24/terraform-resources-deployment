6d4059ecdfba4501eb01f76a0cf4dd89b2cf7414
StefanC24 <stefancepeleaga@gmail.com>
2026-01-16 00:12:47 +0000

    added the remote state

--- provider.tf
+++ provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "4.57.0"
     }
   }
+  backend "azurerm" {
+      resource_group_name  = "fortfstate"
+      storage_account_name = "itcstafortfstate"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
